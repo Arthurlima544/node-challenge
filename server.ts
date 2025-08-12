dcc1724b6bfd19f8a63ddd2ba46afd957c0b5704
Arthurlima544 <arthurlindaolima544@gmail.com>
2025-08-12 18:17:43 -0300

    feat(api): add pino-pretty logger config

--- server.ts
+++ server.ts
@@ -2,7 +2,17 @@ import fastify from 'fastify'
 import crypto from 'node:crypto'
 import multipart from '@fastify/multipart'
 
-const server = fastify()
+const server = fastify({
+    logger: {
+        transport: {
+            target: 'pino-pretty',
+            options: {
+                translateTime: 'HH:MM:ss Z',
+                ignore: 'pid, hostname',
+            },
+        },
+    },
+})
 server.register(multipart)
 
 const courses = [
