773519338b72814a3027f80ca9d0e3a28a55aafe
Arthurlima544 <arthurlindaolima544@gmail.com>
2025-08-12 15:56:31 -0300

    feat(api): implement server with fastify, config for REST Client extension.
    - REST Client extension enable fast usage of requests in your current IDE.
    - Fastify server with '/courses' route resource and simple logic of get courses and post a new course.

--- server.js
+++ server.js
@@ -1,16 +1,23 @@
-const http = require('node:http')
+const fastify = require('fastify')
+const crypto = require('crypto')
 
-const server = http.createServer((request, reply) => {
-    reply.write('Hello world')
-    if (request.url == '/users' && request.method == 'GET') {
-        reply.write('List users...')
-    } else if (request.url == '/users' && request.method == 'POST') {
-        reply.write('Create user...')
-    }// and goes on ...
+const server = fastify()
 
-    reply.end()
+const courses = [
+    { id: 1, title: 'Node.js Course' },
+    { id: 2, title: 'React Course' },
+    { id: 3, title: 'Flutter Course' },
+]
+server.get('/courses', () => {
+    return { courses }
 })
 
-server.listen(3333).on('listening', () => {
+server.post('/courses', (_, reply) => {
+    const courseId = crypto.randomUUID()
+    courses.push({ id: courseId, title: 'New Course' })
+    return reply.status(201).send({ courseId })
+})
+
+server.listen({ port: 3333 }).then(() => {
     console.log('HTTP server running!')
 })
