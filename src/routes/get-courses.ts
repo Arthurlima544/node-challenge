28b9f205d4690d999f13c5f9d401a0232f1365d5
Arthurlima544 <arthurlindaolima544@gmail.com>
2025-08-14 08:14:45 -0300

    feat(api): add filter/ordenation on getCoursesRoute

--- src/routes/get-courses.ts
+++ src/routes/get-courses.ts
@@ -2,6 +2,7 @@ import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod';
 import { db } from "../database/client.ts"
 import { courses } from "../database/schema.ts"
 import z from 'zod';
+import { asc, ilike } from 'drizzle-orm';
 
 export const getCoursesRoute: FastifyPluginAsyncZod = async function (server) {
     server.get('/courses', {
@@ -18,14 +19,25 @@ export const getCoursesRoute: FastifyPluginAsyncZod = async function (server) {
                         })
                     )
                 }).describe('Courses retrieved with success!')
-            }
+            },
+            querystring: z.object({
+                search: z.string().optional().describe('Search a title using Case-insensitive'),
+                orderBy: z.enum(['id', 'title']).optional().default('id').describe('Order ascendant by id or title')
+            })
         }
     }, async (request, reply) => {
+
+        const { search, orderBy } = request.query
+
         const result = await db.select({
             id: courses.id,
             title: courses.title
-        }).from(courses)
-
+        })
+            .from(courses)
+            .where(
+                search ? ilike(courses.title, `%${search}%`) : undefined
+            )
+            .orderBy(asc(courses[orderBy]));
         return reply.send({ courses: result })
     })
 }
