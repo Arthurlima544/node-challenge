aaacb688f664f56e6d103b05af3edde5b3971c05
Arthurlima544 <arthurlindaolima544@gmail.com>
2025-08-14 13:57:04 -0300

    refact(api): separate app and server

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,11 @@
 import fastify from 'fastify'
 import { validatorCompiler, serializerCompiler, type ZodTypeProvider, jsonSchemaTransform } from 'fastify-type-provider-zod'
 import { fastifySwagger } from '@fastify/swagger'
-import { createCoursesRoute } from './src/routes/create-course.ts'
-import { getCoursesByIdRoute } from './src/routes/get-course-by-id.ts'
-import { getCoursesRoute } from './src/routes/get-courses.ts'
+import { createCoursesRoute } from './routes/create-course.ts'
+import { getCoursesByIdRoute } from './routes/get-course-by-id.ts'
+import { getCoursesRoute } from './routes/get-courses.ts'
 import scalarAPIReference from '@scalar/fastify-api-reference'
+
 const server = fastify({
     logger: {
         transport: {
@@ -44,6 +45,5 @@ server.register(createCoursesRoute)
 server.register(getCoursesByIdRoute)
 server.register(getCoursesRoute)
 
-server.listen({ port: 3333 }).then(() => {
-    console.log('HTTP server running!')
-})
+
+export { server }
