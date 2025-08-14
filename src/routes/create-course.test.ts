50994e8d560b1b0042da9ddafe26c88a4a9fb96d
Arthurlima544 <arthurlindaolima544@gmail.com>
2025-08-14 14:34:09 -0300

    feat(test): add create course test

---
+++ src/routes/create-course.test.ts
@@ -0,0 +1,21 @@
+import { test, expect } from 'vitest'
+import supertest from 'supertest'
+import { server } from '../app.ts'
+import { faker } from '@faker-js/faker/locale/pt_BR'
+
+
+test('create a course with success', async () => {
+    await server.ready()
+
+    const response = await supertest(server.server)
+        .post('/courses')
+        .set('Content-Type', 'application/json')
+        .send({ title: faker.commerce.department() })
+
+    console.log(response.body)
+
+    expect(response.status).toEqual(201)
+    expect(response.body).toEqual({
+        courseId: expect.any(String)
+    })
+})
