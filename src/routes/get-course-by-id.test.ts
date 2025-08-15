5683e582152b7aa7bcec4b157b51986f68ebdcc7
Arthurlima544 <arthurlindaolima544@gmail.com>
2025-08-15 11:50:17 -0300

    fix(tests): add authenticated user

--- src/routes/get-course-by-id.test.ts
+++ src/routes/get-course-by-id.test.ts
@@ -2,15 +2,19 @@ import { test, expect } from 'vitest'
 import supertest from 'supertest'
 import { server } from '../app.ts'
 import { makeCourse } from '../tests/factories/make-course.ts'
+import { makeAuthenticatedUser } from '../tests/factories/make-authenticated-user.ts'
 
 
 test('get course by id', async () => {
     await server.ready()
 
+    const { user, token } = await makeAuthenticatedUser('student')
     const course = await makeCourse()
 
     const response = await supertest(server.server)
         .get(`/courses/${course.id}`)
+        .set('Authorization', token)
+
 
     expect(response.status).toEqual(200)
     expect(response.body).toEqual({
@@ -24,9 +28,12 @@ test('get course by id', async () => {
 
 test('return 404 for non existing course by valid uuid', async () => {
     await server.ready()
+    const { user, token } = await makeAuthenticatedUser('student')
 
     const response = await supertest(server.server)
         .get(`/courses/6e3b2494-84c2-44bd-a154-9eda02697230`)
+        .set('Authorization', token)
+
 
     expect(response.status).toEqual(404)
 })
